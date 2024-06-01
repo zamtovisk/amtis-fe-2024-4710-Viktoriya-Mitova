e7ebabbcd3d51fd9537d804bc4d304a425681074
visk <viki.k.mitova@gmail.com>
2024-06-01 15:58:32 +0300

    test

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-// Продължи с твоя vanilla JavaScript код тук.
+
+fetch('http://localhost:4000/desserts')
+.then(responce => responce.text())
+.then(result => Function(JSON.parse(result)));
+
+async function Function(){
+    let i = 0;
+    document.getElementsByClassName("cake-name").forEach(e => {
+        e.innerHTML = result.deserts[i].name;
+        i++
+    })
+}
